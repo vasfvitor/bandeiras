9bf1793060dec8e367fce7906c1a17b646021f12
vasfvitor <61759797+vasfvitor@users.noreply.github.com>
2023-06-12 18:26:57 -0300

    removed i18n astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import i18n from "astro-i18n";
+
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import path from "path";
@@ -10,7 +10,7 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [i18n(), mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap(), tailwind()],
   vite: {
     resolve: {
       alias: {
