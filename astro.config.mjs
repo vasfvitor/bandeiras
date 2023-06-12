3a5df227ca8b73972af9b9794737db50289c7a6e
vasfvitor <61759797+vasfvitor@users.noreply.github.com>
2023-06-12 14:56:08 -0300

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,21 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+import tailwind from "@astrojs/tailwind";
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
-});
+	integrations: [mdx(), sitemap(), tailwind()],
+	vite: {
+		resolve: {
+		  alias: {
+			'~': path.resolve(__dirname, './src')
+		  }
+		},
+		plugins: []
+	  },
+	});
