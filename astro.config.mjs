c3cd5fea6ec174b018b6c3607487725725f50e80
vasfvitor <61759797+vasfvitor@users.noreply.github.com>
2023-06-12 18:05:20 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,22 @@
-import i18n from "astro-i18n"
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import i18n from "astro-i18n";
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import path from "path";
+import { fileURLToPath } from "url";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 import tailwind from "@astrojs/tailwind";
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [i18n(),mdx(), sitemap(), tailwind()],
-	vite: {
-		resolve: {
-		  alias: {
-			'~': path.resolve(__dirname, './src')
-		  }
-		},
-		plugins: []
-	  },
-	});
+  site: "https://example.com",
+  integrations: [i18n(), mdx(), sitemap(), tailwind()],
+  vite: {
+    resolve: {
+      alias: {
+        "~": path.resolve(__dirname, "./src"),
+      },
+    },
+    plugins: [],
+  },
+});
