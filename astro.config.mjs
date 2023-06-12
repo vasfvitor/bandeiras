6912ea11830a262ca1805c3e15f1475e6ef5e2fa
vasfvitor <61759797+vasfvitor@users.noreply.github.com>
2023-06-12 16:25:27 -0300

    i18n removed + added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,4 @@
+import i18n from "astro-i18n"
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import path from 'path';
@@ -9,7 +10,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
-	integrations: [mdx(), sitemap(), tailwind()],
+	integrations: [i18n(),mdx(), sitemap(), tailwind()],
 	vite: {
 		resolve: {
 		  alias: {
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,4 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+/// <reference path="..\.astro-i18n\generated.d.ts" />
