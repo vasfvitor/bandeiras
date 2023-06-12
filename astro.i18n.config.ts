b043a784e4091d27e6d6ed54b06b979631b558b8
vasfvitor <61759797+vasfvitor@users.noreply.github.com>
2023-06-12 18:06:43 -0300

    i18n

--- astro.i18n.config.ts
+++ astro.i18n.config.ts
@@ -1,9 +1,21 @@
-import { defineAstroI18nConfig } from "astro-i18n"
+import { defineAstroI18nConfig } from "astro-i18n";
 
 export default defineAstroI18nConfig({
-	defaultLangCode: "en",
-	supportedLangCodes: [],
-	showDefaultLangCode: false,
-	translations: {},
-	routeTranslations: {},
-})
+  defaultLangCode: "en",
+  supportedLangCodes: ["br"],
+  showDefaultLangCode: true,
+  translations: {
+    br: {
+      About: "Sobre",
+      Flags: "Bandeiras",
+      Home: "Início",
+    },
+  },
+  routeTranslations: {
+    br: {
+      about: "sobre",
+      flags: "bandeiras",
+      home: "inicio",
+    },
+  },
+});
