8fd587c296eb4bcfb55a841a348087caccec7796
vasfvitor <61759797+vasfvitor@users.noreply.github.com>
2023-06-13 21:34:36 -0300

    supabase phase 2

---
+++ src/supa.ts
@@ -0,0 +1,22 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://qqgxhreezrulyhfbtmaj.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFxZ3hocmVlenJ1bHloZmJ0bWFqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODY2OTI5MjIsImV4cCI6MjAwMjI2ODkyMn0.22tytzqJcL7oIkPDLjsEaQGbPGUSefk9EHfTOc9IXIM";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+const { data: db, error } = await supabase.from("UF").select("*");
+
+if (error) {
+  throw new Error(`Erro ao obter dados do banco de dados: ${error}`);
+}
+
+export { db };
+
+function changeText(i: number) {
+  let lang = document.getElementById("lang");
+  db.value = db[i].UF;
+  return db.value;
+}
+//console.log('uf:', ufs);
+//console.log('error:', error);
