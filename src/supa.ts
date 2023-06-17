36a19ca437b7a1e6c401b70045b7fac5fda16956
vasfvitor <61759797+vasfvitor@users.noreply.github.com>
2023-06-17 14:15:24 -0300

    Update supa.ts

--- src/supa.ts
+++ src/supa.ts
@@ -3,20 +3,27 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = "https://qqgxhreezrulyhfbtmaj.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFxZ3hocmVlenJ1bHloZmJ0bWFqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODY2OTI5MjIsImV4cCI6MjAwMjI2ODkyMn0.22tytzqJcL7oIkPDLjsEaQGbPGUSefk9EHfTOc9IXIM";
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    autoRefreshToken: false,
+    persistSession: false,
+  },
+});
 
 const { data: db, error } = await supabase.from("UF").select("*");
 
 if (error) {
   throw new Error(`Erro ao obter dados do banco de dados: ${error}`);
 }
-
+/*
+let { data: reg } = await supabase.from("UF").select(`
+    Region,
+    Regions (
+      region_br
+    )
+  `);
+console.log(reg[1].Regions.region_br);
+*/
 export { db };
-
-function changeText(i: number) {
-  let lang = document.getElementById("lang");
-  db.value = db[i].UF;
-  return db.value;
-}
 //console.log('uf:', ufs);
 //console.log('error:', error);
