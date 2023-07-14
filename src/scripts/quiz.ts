d7f0686fb6b472f686f96715f3aaf27b62302f4a
vasfvitor <61759797+vasfvitor@users.noreply.github.com>
2023-07-14 13:52:28 -0300

    versão 2 quiz

--- src/scripts/quiz.ts
+++ src/scripts/quiz.ts
@@ -1,6 +1,13 @@
 import db from "~/pages/uf/estados.json";
 
-function ufList() {
+interface TheQuiz {
+  q: number;
+  guesses: number;
+  sequence: number[];
+  currentAns: number;
+}
+
+function getUfs() {
   const uf = db?.map((db) => db.UF);
   const uf_name = db?.map((db) => db.Name);
   const ufs = { short: uf, long: uf_name };
@@ -24,23 +31,39 @@ function randomNums(n: number): number[] {
 //
 //
 window.onload = function () {
-  document.getElementById("q-restart")?.addEventListener("click", start);
+  document.getElementById("q-restart")?.addEventListener("click", reset);
   //
-  function start() {
-    let ufsArray = [...ufs.short];
-    let rndInt: number[] = randomNums(27);
-    let shuffledFlags = shuffle(ufsArray);
+  let theGame: TheQuiz = {
+    q: 0,
+    guesses: 0,
+    sequence: randomNums(27),
+    currentAns: 0,
+  };
+
+  function start(): TheQuiz {
+    let shuffledFlags = shuffle([...ufs.short]);
     placeFlags(shuffledFlags);
-    let game = { q: 0, guesses: 0, right: 0, questionSeq: rndInt };
-    console.log(game);
-    generateQuestion(game.questionSeq[game.q]);
-    return game;
+    randomizeQuestion();
+    return theGame;
   }
 
-  //console.log(`${ufs.short[4]} + ${ufs.long[4]}`);
+  function reset() {
+    theGame.guesses = 0;
+    console.log("before = " + theGame.currentAns);
+    randomizeQuestion();
+    console.log("after  = " + theGame.currentAns);
+    restartFlags();
+    return theGame;
+  }
+
+  function randomizeQuestion() {
+    theGame.sequence = randomNums(27);
+    theGame.currentAns = generateQuestion(theGame.sequence.shift());
+  }
 
   function placeFlags(shuffledFlags: string[]) {
     if (!document.getElementById(shuffledFlags[0])) {
+      const div = document.getElementById("q-bandeiras");
       for (const UF of shuffledFlags) {
         const flag = document.createElement("img");
         flag.className =
@@ -49,67 +72,58 @@ window.onload = function () {
         flag.alt = `Bandeira ${UF}`;
         flag.id = UF;
         flag.addEventListener("click", guess);
-        document.getElementById("q-bandeiras")?.appendChild(flag);
+        div?.appendChild(flag);
       }
-    } else {
-      let flags = Array.from(document.getElementsByClassName("q-flag"));
-      flags.forEach((flag) => {
-        flag.classList.remove("q-tip", "q-disabled", "q-tip-animation");
-        flag.addEventListener("click", guess);
-      });
-      /* for (let i = 0; i < flags.length; i++) {
-        let flag = flags[i];
-        flag.classList.remove("q-tip");
-        flag.classList.remove("q-disabled");
-        flag.classList.remove("q-tip-animation");
-        flag.addEventListener("click", guess);
-      } */
     }
+  }
+
+  function restartFlags() {
+    let flags = Array.from(document.getElementsByClassName("q-flag"));
+    flags.forEach((flag) => {
+      flag.removeEventListener("click", guess);
+      flag.classList.remove("q-tip", "q-disabled", "q-tip-animation");
+      flag.addEventListener("click", guess);
+    });
   }
 
   function generateQuestion(idx: number | undefined): number {
     if (idx === undefined) {
-      document.getElementById("question").textContent = `Fim de jogo`;
+      document.getElementById("q-question").textContent = `Fim de jogo`;
       endGame();
       return 0;
     }
     const p = document.getElementById("q-question");
     p.textContent = `Clique na bandeira: ${ufs.long[idx]}`;
-    //console.log(`${ufs.short[idx]} - ${ufs.long[idx]} - ${idx}`);
     return idx;
   }
-
   //
   //
-  //
-  const ufs = ufList();
-  let game = start();
-  //
-  //
+  const ufs = getUfs();
+  start();
   //
   //
   function guess(this: any) {
-    //console.log("current:" + questionFlag);
-    //console.log(rndInt.length);
-    //console.log(this.id + ufs.long[currentQuestion]);
+    console.log(theGame.currentAns);
     const tryFlagId = this.id;
-    const ansFlagId = ufs.short[game.q];
-    const ansFlag = document.getElementById(ansFlagId);
-    if (!ansFlag) {
+    const ansFlagId = ufs.short[theGame.currentAns];
+    const ansFlag = document.getElementById(ansFlagId) as HTMLImageElement;
+    /*if (!ansFlag) {
       return;
-    } else if (tryFlagId === ansFlagId) {
-      document.getElementById(tryFlagId)?.removeEventListener("click", guess);
-      game.guesses = 0;
-      game.q = generateQuestion(game.questionSeq.shift());
-      //console.log(game.questionSeq.length);
+    } else
+    */
+
+    if (tryFlagId === ansFlagId) {
+      this.removeEventListener("click", guess);
+      theGame.guesses = 0;
+      theGame.currentAns = generateQuestion(theGame.sequence.shift());
+      console.log("curr = " + theGame.currentAns);
       ansFlag.classList.remove("q-tip", "q-tip-animation");
       ansFlag.classList.add("q-disabled");
-    } else if (game.guesses === 4) {
-      game.guesses = 0;
+    } else if (theGame.guesses === 4) {
+      theGame.guesses = 0;
       ansFlag.classList.add("q-tip", "q-tip-animation");
     }
-    game.guesses++;
-    return game.guesses;
+    theGame.guesses++;
   }
 
   function endGame() {
