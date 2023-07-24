c2c5b6e25ef94c81b81d6f288d750fe5260015f8
vasfvitor <61759797+vasfvitor@users.noreply.github.com>
2023-07-24 16:44:55 -0300

    Update BR-map.ts

--- src/scripts/BR-map.ts
+++ src/scripts/BR-map.ts
@@ -8,78 +8,87 @@ function load_map() {
   const toolTip = document.getElementById("toolTip") as HTMLDivElement;
   const mapWidth = document.getElementById("map")?.offsetWidth;
   let toolTipW, toolTipH, x_offset, y_offset;
-  // , region, currentRegion;
 
-  // Add event listeners to map element
   if (
     !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
       navigator.userAgent
     )
   ) {
     // Mouse events
-    map.addEventListener("click", handleClick, false);
+    //map.addEventListener("click", handleClick, false);
     map.addEventListener("mousemove", mouseEntered, false);
     map.addEventListener("mouseout", mouseGone, false);
   } else {
     // Touch events
     map.addEventListener("touchstart", touchStart, false);
-    map.addEventListener("touchmove", touchStart, false);
-    //map.addEventListener("touchend", mobileGone, false);
+    map.addEventListener("touchmove", touchMove, false);
+    map.addEventListener("touchend", mobileGone, false);
   }
   let active = false;
-  let id = 0;
+  const cardTimeouts: { [key: string]: number } = {};
 
   function touchStart(e: TouchEvent) {
     mouseEntered(e);
-    id = window.setTimeout(() => {
-      mouseGone(e);
-    }, 200);
   }
-  /*
-  const pathElement = document.getElementById("path8");
-  if (pathElement instanceof SVGElement) {
-    const postalAttribute = pathElement.getAttribute("postal");
-    if (postalAttribute !== null) {
-      // The value of the postal attribute
-      console.log(postalAttribute);
-    } else {
-      // The postal attribute is not present
-      console.log("Postal attribute not found");
+  function touchMove(e: TouchEvent) {
+    mouseEntered(e);
+  }
+  //
+  function mobileGone(e: TouchEvent) {
+    const target = e.target as SVGPathElement;
+    const cardId: string = target.getAttribute("postal") || "error";
+    if (cardId == "error") {
+      console.log(cardId);
     }
-  } else {
-    // Element with id "path8" is not an SVG element
-    console.log("Element with id 'path8' is not an SVG element");
+    active = true;
+    const timeoutId = window.setTimeout(() => {
+      mouseGone(e);
+    }, 1000);
+    if (!active) {
+      mouseGone(e);
+    }
+    cardTimeouts[cardId] = timeoutId;
   }
-*/
 
+  function cancelTimeout(cardId: string) {
+    // Verifique se o card tem um timeout ativo e cancele-o
+    const timeoutId = cardTimeouts[cardId];
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+      // Remova o timeout do objeto após cancelá-lo para liberar recursos
+      delete cardTimeouts[cardId];
+    }
+  }
   // Show tooltip on mousemove
   function mouseEntered(e: MouseEvent | TouchEvent) {
     //console.log("E   " + e);
-    let target = e.target as SVGPathElement;
+    const target = e.target as SVGPathElement;
     if (target.nodeName === "path") {
       target.style.cursor = "pointer";
       const region = target.getAttribute("region");
       const postal = target.getAttribute("postal");
       const name = target.getAttribute("name");
-      const event = e.constructor.name;
-      event == "MouseEvent";
+
       // Paint according to each region
       paintRegion(region, target);
-      if (e instanceof MouseEvent) {
+
+      // for some reason this does not work:
+      // if (e instanceof MouseEvent)
+      // even tho e.constructor.name === "MouseEvent"
+      const event = e.constructor.name;
+      if (event === "MouseEvent") {
         toolTipW = toolTip.offsetWidth;
         toolTipH = toolTip.offsetHeight;
-
         y_offset = e.offsetY - toolTipH - 20;
         x_offset = e.offsetX - toolTipW / 2;
-        // CSS
-
-        // Tooltip data
       } else {
         y_offset = 0;
         x_offset = 0;
       }
+      // CSS
       toolTip.style.transform = `translate(${x_offset}px, ${y_offset}px)`;
       toolTip.classList.remove("toolTip-hidden");
+      // Tooltip data
       toolTip.innerHTML = `
           <p><strong>${name} - ${postal}</strong></p>
       `;
@@ -87,23 +96,24 @@ function load_map() {
   }
   // Remove tooltip on mouseout
   function mouseGone(e: MouseEvent | TouchEvent) {
-    let target = e.target;
+    let target = e.target as SVGPathElement;
     if (target.nodeName === "path") {
       target.style.fill = "black";
-      target.style.opacity = 1;
+      target.style.opacity = "1";
       toolTip.classList.add("toolTip-hidden");
       //toolTip.innerHTML = "";
     }
   }
   // Go to uf page onclick
   function handleClick(e: MouseEvent | TouchEvent) {
-    if (e.target.nodeName === "path") {
-      let details = e.target.attributes;
-      window.location.href = `/uf/${details.postal.value}`;
+    const target = e.target as SVGPathElement;
+    if (target.nodeName === "path") {
+      const postal = target.getAttribute("postal");
+      window.location.href = `/uf/${postal}`;
     }
   }
 
-  function paintRegion(region: string | null, t) {
+  function paintRegion(region: string | null, t: SVGPathElement) {
     switch (region) {
       case "North":
         t.style.fill = "#a99ada";
@@ -121,7 +131,7 @@ function load_map() {
         t.style.fill = "#fb9a99";
         break;
       default:
-        t.style.opacity = 0.6;
+        t.style.opacity = "0.6";
         break;
     }
   }
@@ -129,15 +139,3 @@ function load_map() {
 
 // Calls init function on window load
 window.addEventListener("load", load_map);
-
-//
-function mobileGone(e: MouseEvent | TouchEvent) {
-  active = true;
-  id = window.setTimeout(() => {
-    mouseGone(e);
-  }, 1000);
-  if (!active) {
-    mouseGone(e);
-  }
-}
-//
