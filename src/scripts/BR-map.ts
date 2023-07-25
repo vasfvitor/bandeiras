927087c0c2089b814b62a7c97a3fda119361c9d7
vasfvitor <61759797+vasfvitor@users.noreply.github.com>
2023-07-25 17:40:33 -0300

    mobile tooltip

--- src/scripts/BR-map.ts
+++ src/scripts/BR-map.ts
@@ -17,72 +17,61 @@ function load_map() {
     document.getElementById("mobile-tip")?.classList.remove("hidden");
     // Touch events
     map.addEventListener("touchstart", touchEnter, false);
-    map.addEventListener("touchmove", touchEnter, false);
-    map.addEventListener("touchend", mobileGone, false);
-  }
-
-  let active = false;
-
-  function createTooltip(name: string | null, postal: string | null) {
-    if (document.getElementById(`${postal}-tooltip`)) {
-      document
-        .getElementById(`${postal}-tooltip`)
-        ?.classList.remove("tooltip-hidden");
-    } else {
-      const f = document.createElement("div");
-      f.classList.add("tooltip-bew");
-      f.id = `${postal}-tooltip`;
-      f.innerHTML = `
-    <p><strong>${name} - ${postal}</strong></p>
-`;
-      document.getElementById("map-wrapper")?.appendChild(f);
-      window.setTimeout(() => {
-        hideTooltip(f);
-      }, 1000);
-    }
-  }
-
-  function hideTooltip(t: HTMLDivElement) {
-    t.classList.add("tooltip-hidden");
-  }
-
-  //
-
-  function mobileGone(e: TouchEvent) {
-    active = true;
-    window.setTimeout(() => {
-      mobile_Gone(e);
-    }, 1000);
-    //
-    if (!active) {
-      mobile_Gone(e);
-    }
-  }
-  function mobile_Gone(e: MouseEvent | TouchEvent) {
-    let target = e.target as SVGPathElement;
-    if (target.nodeName === "path") {
-      target.style.fill = "black";
-      target.style.opacity = "1";
-    }
+    //map.addEventListener("touchmove", touchEnter, false);
+    map.addEventListener("touchend", touchEnd, false);
   }
 
   function touchEnter(e: TouchEvent) {
     const target = e.target as SVGPathElement;
     if (target.nodeName === "path") {
-      target.style.cursor = "pointer";
       const region = target.getAttribute("region");
       const postal = target.getAttribute("postal");
       const name = target.getAttribute("name");
       createTooltip(name, postal);
-      // Paint according to each region
       paintRegion(region, target);
-      y_offset = 0;
-      x_offset = 0;
+    }
+  }
+  let i = 5;
+  function createTooltip(name: string | null, postal: string | null) {
+    if (i > 999) {
+      i = 5;
+    }
+    i++;
+    const t = document.getElementById(`${postal}-tooltip`) as HTMLDivElement;
+    if (t) {
+      t.classList.remove("tooltip-hidden");
+      t.style.zIndex = `${i}`;
+    } else {
+      const f = document.createElement("div");
+      f.classList.add("tooltip-mob");
+      f.innerHTML = `
+      <p><strong>${name} - ${postal}</strong></p>
+      `;
+      f.id = `${postal}-tooltip`;
+      f.style.zIndex = `${i}`;
+      document.getElementById("map-wrapper")?.appendChild(f);
     }
   }
 
-  // MOUSE EVENTS
+  function hideTooltip(t: HTMLDivElement) {
+    t.classList.add("tooltip-hidden");
+    t.style.zIndex = "10";
+  }
 
+  function touchEnd(e: TouchEvent) {
+    window.setTimeout(() => {
+      const target = e.target as SVGPathElement;
+      const postal = target.getAttribute("postal");
+      const t = document.getElementById(`${postal}-tooltip`) as HTMLDivElement;
+      if (target.nodeName === "path") {
+        hideTooltip(t);
+        target.style.fill = "black";
+        target.style.opacity = "1";
+      }
+    }, 250);
+  }
+
+  // MOUSE EVENTS
   // Remove tooltip on mouseout
   function mouseGone(e: MouseEvent | TouchEvent) {
     let target = e.target as SVGPathElement;
@@ -104,19 +93,13 @@ function load_map() {
       const region = target.getAttribute("region");
       const postal = target.getAttribute("postal");
       const name = target.getAttribute("name");
-
-      // Paint according to each region
       paintRegion(region, target);
-
       tooltipW = tooltip.offsetWidth;
       tooltipH = tooltip.offsetHeight;
       y_offset = e.offsetY - tooltipH - 20;
       x_offset = e.offsetX - tooltipW / 2;
-
-      // CSS
       tooltip.style.transform = `translate(${x_offset}px, ${y_offset}px)`;
       tooltip.classList.remove("tooltip-hidden");
-      // tooltip data
       tooltip.innerHTML = `
           <p><strong>${name} - ${postal}</strong></p>
       `;
